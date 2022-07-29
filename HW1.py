0ae1983c00e525fd9894a11bb477b907fb1db336
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2022-07-29 11:16:01 -0400

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,18 @@
+# CSC 122
+# HW1
+# Pay Raise Calculator Program
+
+"""
+Inputs, Processes, Outputs (IOP)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Input(s):
+Processes:
+Output(s):
+"""
+
+def main():
+    pass # replace this line with all of your solution code
+
+if __name__ == "__main__":
+    main()
+    
